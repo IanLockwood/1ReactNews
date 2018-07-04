ebc28843359b2a9b88711834cd696b588ddc5ec3
IanLockwood <mrianlockwood@gmail.com>
2018-07-04 12:47:47 -0400

    Finish basic search bar

--- src/SearchBar/SearchButton.js
+++ src/SearchBar/SearchButton.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class SearchButton extends Component {
   render() {
     return (
-      <button className="SearchButton"></button>
+      <button className="SearchButton">Search</button>
     );
   }
 }
--- src/SearchBar/SearchSort.js
+++ src/SearchBar/SearchSort.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export class SearchSort extends Component {
   render() {
     return (
-      <select className="SearchSort"></select>
+      <select className="SearchSort">
+        <option>Sort Articles</option>
+      </select>
     );
   }
 }
